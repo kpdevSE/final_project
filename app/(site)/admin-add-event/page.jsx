07751e107c3624de26241a4796f72e2844049bfb
kanishkapasi <kanishkapasindu6@gmail.com>
2024-01-19 15:22:51 +0530

    'kpdev'

--- app/(site)/admin-add-event/page.jsx
+++ app/(site)/admin-add-event/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { UploadDropzone } from "@uploadthing/react";
+import Link from "next/link";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
@@ -7,7 +8,7 @@ export default function AdminAddEvent() {
   const [imageUrl, setImageUrl] = useState("");
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <form className=" mx-auto w-[55%] mt-48">
+      <form className=" mx-auto w-[85%] lg:w-[55%] lg:mt-48 mt-64">
         <UploadDropzone
           endpoint="imageUploader"
           onClientUploadComplete={(res) => {
@@ -37,21 +38,7 @@ export default function AdminAddEvent() {
             Email address
           </label>
         </div>
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="password"
-            name="floating_password"
-            id="floating_password"
-            className="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            for="floating_password"
-            className="peer-focus:font-medium absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-            Password
-          </label>
-        </div>
+
         <div className="relative z-0 w-full mb-5 group">
           <input
             type="password"
@@ -113,7 +100,7 @@ export default function AdminAddEvent() {
             <label
               for="floating_phone"
               className="peer-focus:font-medium absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-              Phone number (123-456-7890)
+              Phone number
             </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
@@ -146,6 +133,35 @@ export default function AdminAddEvent() {
             className="peer-focus:font-medium absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
             Description
           </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group flex justify-center items-center">
+          <div className="dropdown dropdown-top">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn m-1 text-lg hover:bg-[#01a2b7] p-2 hover:border hover:rounded-lg hover:text-white bg-white border-[#01a2b7] border shadow-md shadow-[#8ceaf7] w-[100%]">
+              Category
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+              <li className="hover:bg-[#01a2b7] rounded-lg hover:text-white">
+                <Link href={"/admin-login"}>DJs</Link>
+              </li>
+              <li className="hover:bg-[#01a2b7] rounded-lg hover:text-white">
+                <Link href={"/register"}>Get Togethers</Link>
+              </li>
+              <li className="hover:bg-[#01a2b7] rounded-lg hover:text-white">
+                <Link href={"/vendor-register"}>Weddings</Link>
+              </li>
+              <li className="hover:bg-[#01a2b7] rounded-lg hover:text-white">
+                <Link href={"/register"}>Birthday Parties</Link>
+              </li>
+              <li className="hover:bg-[#01a2b7] rounded-lg hover:text-white">
+                <Link href={"/vendor-register"}>Live in Concerts</Link>
+              </li>
+            </ul>
+          </div>
         </div>
         <button
           type="submit"
