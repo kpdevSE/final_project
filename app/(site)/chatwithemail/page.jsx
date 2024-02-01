fc5811ccb78fc36e90505ba3e25f9fe441774b2a
kpdevSE <kanishkapasindu6@gmail.com>
2024-02-01 09:47:18 +0530

    kpdev

--- app/(site)/chatwithemail/page.jsx
+++ app/(site)/chatwithemail/page.jsx
@@ -9,9 +9,11 @@ export default function ChatWithEmail() {
   const [to, setTo] = useState();
   const [subject, setSubject] = useState();
   const [message, setMessage] = useState();
+  const [loading, setloading] = useState(true);
 
   const onSubmitHandler = async () => {
     // e.preventDefault();
+
     const response = await fetch("api/sendMail", {
       method: "POST",
       headers: {
@@ -23,6 +25,7 @@ export default function ChatWithEmail() {
         if (response.ok) {
           toast.success("Email Send Successfully");
         }
+        setloading(false);
       })
       .catch((error) => {
         toast.error("Something went wrong ", error);
@@ -132,7 +135,7 @@ export default function ChatWithEmail() {
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Send Message
+            {!loading ? "Please Wait" : "Send a Message"}
           </button>
         </form>
       </div>
